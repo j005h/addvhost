ccf30ac6f9e9f6f24ee08d72f047fddb4fdb08d1
j005h <31870661+j005h@users.noreply.github.com>
2017-09-11 22:09:38 -0300

    Create addvhost.sh

---
+++ addvhost.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Script para atualizaçao, busca e criaçao de vhost
+# Feito por Olimpio Freitas 23/05/2007
+
+
+VHOST_DIR=/home/v0id/
+
+MENSAGEM_USO="
+Uso: $(basename "$0") [OPÇÕES]
+
+OPÇÕES:
+  -r, --reverse     Inverte a listagem
+  -s, --sort        Ordena a listagem alfabeticamente
+  -u, --uppercase   Mostra a listagem em MAIÚSCULAS
+
+  -h, --help        Mostra esta tela de ajuda e sai
+  -V, --version     Mostra a versão do programa e sai
+"
+
+
+case "$1" in
+
+	-c | --criar)
+		echo -e "Digite o FQDN: "
+		read $FQDN
+		echo -e "Digite o path para aplicacao: "
+		read $FILE_PATH
+		echo -n "<VirtualHost *:80> \
+				 DocumentRoot ('$FILE_PATH')\
+				 ServerName "$FQDN"\
+   				 RedirectMatch 404 /\\.svn(/|$)\
+  				 <LocationMatch "^.*$">\
+      			 	 SecRuleRemoveById 958030 960015 970903 981203 981246 959073 950001 981257 \
+  				 </LocationMatch>\
+			  </VirtualHost>" >> VHOST_DIR
+	;;
+
+
+
+	-h | --help)
+		echo "$MENSAGEM_USO"
+		exit 0
+	;;
+
+	-V | --version)
+		echo -n $(basename "$0")
+		# Extrai a versão diretamente dos cabeçalhos do programa
+		grep '^# Versão ' "$0" | tail -1 | cut -d : -f 1 | tr -d \#
+		exit 0
+	;;
+
+	*)
+		if test -n "$1"
+		then
+			echo Opção inválida: $1
+			exit 1
+		fi
+	;;
+
+
+esac
